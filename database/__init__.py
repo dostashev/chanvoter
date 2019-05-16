b22e5c4f0d2ca936405d7c650b55bd7d87ab1e20
D34DStone <torianik.online@tutanota.com>
2019-05-16 20:29:57 +0300

    Write docs.

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from database.models import * 
 
-def get_db_console(db_path):
+"""
+USAGE EXAMPLE:
+
+
+from database import open_db
+from database import User, Transaction
+
+# open db and get handle functions
+scope, clear = open_db('local_or_global_path_to_my_db')
+
+# let create an user
+user1 = User(public_key='438fxyfx4g9n2zdfdn258f2', ...)
+
+# open session
+with scope() as session:
+    session.add(user1)
+
+# You don't need to commit changes in db, session manager in case of success manage it
+"""
+
+def open_db(db_path):
     engine = create_engine(f'sqlite:///{db_path}')
     Session = sessionmaker(bind=engine)
 
