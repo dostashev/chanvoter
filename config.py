637c86c2f35866bd01723ab70ea5644e8c039308
D34DStone <torianik.george@tutanota.com>
2019-06-07 21:38:30 +0300

    Add logo.

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     COINS_PER_VOTE = 10_000
 
     ADMIN_PASS = "admin"
 
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
     FLASK_CONFIG = {
                 "TESTING" : True, 
                 "DEBUG" : True, 
                 "SECRET_KEY" : "this_is_realy_secret",
-                "SQLALCHEMY_DATABASE_URI" : os.path.join(basedir, "var", "main.db"),
+                "SQLALCHEMY_DATABASE_URI" : os.path.join(BASE_DIR, "var", "main.db"),
             }
    
     
