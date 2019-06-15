d9f825ac874dcb88f59868f4d1f1d45b3f0d03b2
D34DStone <torianik.george@tutanota.com>
2019-06-15 15:01:38 +0300

    add schemas

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -1,12 +1,47 @@
 from marshmallow import Schema, fields, pprint
 
+class UserSchema(Schema):
+    id = fields.Int()
+    address = fields.Str()
+    private_key = fields.Str()
+    coins = fields.Int()
+
+
 class GirlSchema(Schema):
+    id = fields.Int()
     name = fields.Str()
+    instagram = fields.Str()
+    photo = fields.Str()
+    ELO = fields.Float()
 
-class hello():
-    name = "123"
 
-if __name__ == "__main__":
-    gs = GirlSchema()
-    h = hello()
-    print(gs.dump(h).data)
+class ContestSchema(Schema):
+    id = fields.Int()
+    first_girl_id = fields.Int()
+    first_girl = fields.Nested(GirlSchema)
+    second_girl_id = fields.Int()
+    second_girl = fields.Nested(GirlSchema)
+    begin = fields.DateTime()
+    end   = fields.DateTime()
+    finalized = fields.Bool()
+
+
+class VoteSchema(Schema):
+    id = fields.Int()
+    user_addr = fields.Str()
+    user = fields.Nested(UserSchema)
+    contest_id = fields.Int()
+    contest = fields.Nested(ContestSchema)
+    chosen_id = fields.Int()
+    
+
+class BetScheme(Schema):
+    id = fields.Int()
+    coins = fields.Int()
+    user_addr = fields.Str()
+    user = fields.Nested(UserSchema)
+    contest_id = fields.Int()
+    contest = fields.Nested(ContestSchema)
+    chosen_id = fields.Int()
+    finalized = fields.Bool()
+    profit = fields.Int(default=0)
